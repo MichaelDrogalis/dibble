4e5a2c221a74716ed00e4d29a640028fd65e11f6
Michael Drogalis <mjd3089@rit.edu>
2012-10-31 23:47:52 -0400

    More efficient way to parse the output of MySQL.

--- src/dibble/core.clj
+++ src/dibble/core.clj
@@ -8,18 +8,18 @@
 (defmacro seed [& columns])
 
 (defn mysql-to-clj-type [[column data-type]]
-  (cond (re-matches #"varchar.*" data-type) [(keyword column) :string]
-        (re-matches #"int.*" data-type) [(keyword column) :integer]))
+  (cond (re-matches #"varchar.*" data-type) {(keyword column) :string}
+        (re-matches #"int.*" data-type) {(keyword column) :integer}))
 
 (defn mysql-db [args]
   (let [query (str "show columns from " (name (:table args)))]
     (defdb target-db (mysql (:database args)))
-    (apply hash-map
-           (flatten
-            (map mysql-to-clj-type
-                 (apply hash-map (interleave
-                                  (map :Field (exec-raw query :results))
-                                  (map :Type (exec-raw query :results)))))))))
+    (apply merge
+           (map mysql-to-clj-type
+                (partition 2 2
+                           (interleave
+                            (map :Field (exec-raw query :results))
+                            (map :Type (exec-raw query :results))))))))
 
 (defn seed-table [args]
   (cond (= (:vendor (:database args)) :mysql) (mysql-db args)
