13da1046ed7e20e55c9887e4b1e986554b4f88e5
Michael Drogalis <mjd3089@rit.edu>
2012-10-31 23:44:45 -0400

    Got a working solution for obtaining pairs of column names and data types for MySQL.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [korma "0.3.0-beta9"]
+                 [mysql/mysql-connector-java "5.1.21"]]
+  :main dibble.core)
--- src/dibble/core.clj
+++ src/dibble/core.clj
@@ -1,6 +1,32 @@
-(ns dibble.core)
+(ns dibble.core
+  (:require [korma.core :refer :all]
+            [korma.db :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn randomized [column-name]
+  5)
+
+(defmacro seed [& columns])
+
+(defn mysql-to-clj-type [[column data-type]]
+  (cond (re-matches #"varchar.*" data-type) [(keyword column) :string]
+        (re-matches #"int.*" data-type) [(keyword column) :integer]))
+
+(defn mysql-db [args]
+  (let [query (str "show columns from " (name (:table args)))]
+    (defdb target-db (mysql (:database args)))
+    (apply hash-map
+           (flatten
+            (map mysql-to-clj-type
+                 (apply hash-map (interleave
+                                  (map :Field (exec-raw query :results))
+                                  (map :Type (exec-raw query :results)))))))))
+
+(defn seed-table [args]
+  (cond (= (:vendor (:database args)) :mysql) (mysql-db args)
+        :else (println "Database :vendor not supported")))
+
+(seed-table
+ {:database {:vendor :mysql :db "simulation" :user "root" :password ""}
+  :table :people})
+
+(defn -main [& args])
