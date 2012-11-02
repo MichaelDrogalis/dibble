73a62b1186e9ad24cb7ce9813452081f3046fcb9
Michael Drogalis <mjd3089@rit.edu>
2012-11-01 23:09:09 -0400

    Refactored some of the MySQL type inference code.

--- src/dibble/core.clj
+++ src/dibble/core.clj
@@ -7,14 +7,11 @@
         (re-matches #"int.*" data-type) {(keyword column) :integer}))
 
 (defn mysql-db [args]
-  (let [query (str "show columns from " (name (:table args)))]
+  (let [query (exec-raw (str "show columns from " (name (:table args))) :results)
+        fields (map :Field query)
+        types (map :Type query)]
     (defdb target-db (mysql (:database args)))
-    (apply merge
-           (map mysql-to-clj-type
-                (partition 2 2
-                           (interleave
-                            (map :Field (exec-raw query :results))
-                            (map :Type (exec-raw query :results))))))))
+    (apply merge (map mysql-to-clj-type (partition 2 (interleave fields types))))))
 
 (defn seed-table [args]
   (cond (= (:vendor (:database args)) :mysql) (mysql-db args)
@@ -26,8 +23,5 @@
   (let [data-type (get table-description column)]
     (cond (= data-type :string) "Mike"
           (= data-type :integer) 42)))
-
-(randomized :name description)
-(randomized :number description)
 
 (defn -main [& args])
