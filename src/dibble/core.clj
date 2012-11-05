fa808ee9157b6735eb85ec5cd080ebb5c4d0eaec
Michael Drogalis <mjd3089@rit.edu>
2012-11-05 15:13:17 -0500

    Moved string functionality to its own file.

--- src/dibble/core.clj
+++ src/dibble/core.clj
@@ -7,9 +7,6 @@
 (defn random-integer [low high]
   (first (shuffle (range low (inc high)))))
 
-(defn randomized-string [column]
-  (random-string (random-integer 0 (:max-chars column))))
-
 (defn randomized-integer [column]
   (Integer/parseInt (apply str (map (fn [_] (first (shuffle (range 0 2)))) (range 0 (dec (* (:bytes column) 8))))) 2))
 
--- src/dibble/strings.clj
+++ src/dibble/strings.clj
@@ -1,8 +1,11 @@
 (ns dibble.strings)
 
-(defn random-string [length]
+(defn- random-string [length]
   (let [low-ascii-char 97
         high-ascii-char 122
         infinite-char-seq (repeatedly #(+ (rand-int (- high-ascii-char low-ascii-char)) low-ascii-char))]
     (apply str (map char (take length infinite-char-seq)))))
+
+(defn randomized-string [column]
+  (random-string (random-integer 0 (:max-chars column))))
 
