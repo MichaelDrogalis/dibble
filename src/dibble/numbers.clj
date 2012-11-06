418e83ec2fbebfa3a536410bc395cc100b7167a4
Michael Drogalis <mjd3089@rit.edu>
2012-11-05 20:15:29 -0500

    Options for randomized integers.

--- src/dibble/numbers.clj
+++ src/dibble/numbers.clj
@@ -1,8 +1,12 @@
-(ns dibble.numbers)
+(ns dibble.numbers
+  (import java.util.Random))
 
-(defn random-integer [low high]
-  (first (shuffle (range low (inc high)))))
+(defn random-integer [min max]
+  (+ (.nextInt (new Random) (- (inc max) min)) min))
 
-(defn randomized-integer [column args]
-  (Integer/parseInt (apply str (map (fn [_] (rand-nth [0 1])) (range 0 (dec (* (:bytes column) 8))))) 2))
+(defn randomized-integer [column {:keys [min max]}]
+  (cond (and (not (nil? min)) (not (nil? max))) (random-integer min max)
+        (and (not (nil? min)) (nil? max)) (random-integer min Integer/MAX_VALUE)
+        (and (nil? min) (not (nil? max))) (random-integer Integer/MIN_VALUE max)
+        :else (random-integer Integer/MIN_VALUE Integer/MAX_VALUE)))
 
