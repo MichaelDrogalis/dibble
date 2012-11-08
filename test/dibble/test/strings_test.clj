0f6fa18550d94918be22a1302b0f6de67ab6ce19
Michael Drogalis <mjd3089@rit.edu>
2012-11-08 18:20:33 -0500

    Tests for string max length.

--- test/dibble/test/strings_test.clj
+++ test/dibble/test/strings_test.clj
@@ -12,3 +12,9 @@
 (fact (>= (count (randomized-string column {:min 1})) 1) => true)
 (fact (>= (count (randomized-string column {:min 5})) 5) => true)
 (fact (<= (count (randomized-string column {:min 9})) 3) => false)
+
+(fact (<= (count (randomized-string column {:max 0})) 0) => true)
+(fact (<= (count (randomized-string column {:max 5})) 5) => true)
+(fact (<= (count (randomized-string column {:max 9})) 9) => true)
+
+
