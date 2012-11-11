ea96e06eb7281b9c7f90c7fcf3e1afc9bf53d597
Michael Drogalis <mjd3089@rit.edu>
2012-11-11 14:14:48 -0500

    Floats, decimals, and doubles all parsed uniquely.

--- src/dibble/mysql.clj
+++ src/dibble/mysql.clj
@@ -4,7 +4,9 @@
 
 (def varchar-regex #"varchar\((\d+)\)")
 (def integer-regex #"int.*")
-(def decimal-regex #"(float|double|decimal).*")
+(def float-regex   #"float(\((\d+),(\d+)\))?")
+(def double-regex  #"double(\((\d+),(\d+)\))?")
+(def decimal-regex #"decimal(\((\d+),(\d+)\))?")
 
 (defn varchar-metadata [column description]
   {(keyword column) {:type :string :max-chars (read-string (nth description 1))}})
@@ -12,8 +14,21 @@
 (defn integer-metadata [column description]
   {(keyword column) {:type :integer :bytes 4}})
 
+(defn float-metadata [column description]
+  {(keyword column)
+   {:type :decimal
+    :precision (read-string (or (nth description 2) "23"))}})
+
+(defn double-metadata [column description]
+  {(keyword column)
+   {:type :decimal
+    :precision (read-string (or (nth description 2) "53"))}})
+
 (defn decimal-metadata [column description]
-  {(keyword column) {:type :decimal}})
+  {(keyword column)
+   {:type :decimal
+    :precision (read-string (or (nth description 2) "10"))
+    :accuracy (read-string (or (nth description 3) "0"))}})
 
 (defn mysql-to-clj-type [[column data-type]]
   (first
@@ -25,6 +40,8 @@
          (metadata-fn column description)))
      [[varchar-regex varchar-metadata]
       [integer-regex integer-metadata]
+      [float-regex float-metadata]
+      [double-regex double-metadata]
       [decimal-regex decimal-metadata]]))))
 
 (def connect-to-db (memoize #(default-connection (create-db (mysql %)))))
--- test/dibble/test/mysql_test.clj
+++ test/dibble/test/mysql_test.clj
@@ -11,11 +11,11 @@
 (fact (mysql-to-clj-type [:age "int(5)"]) => {:age {:type :integer :bytes 4}})
 (fact (mysql-to-clj-type [:age "int(11)"]) => {:age {:type :integer :bytes 4}})
 
-(fact (mysql-to-clj-type [:balance "double"]) => {:balance {:type :decimal}})
-(fact (mysql-to-clj-type [:balance "double(5, 4)"]) => {:balance {:type :decimal}})
+(fact (mysql-to-clj-type [:balance "double"]) => {:balance {:type :decimal :precision 53}})
+(fact (mysql-to-clj-type [:balance "double(5,4)"]) => {:balance {:type :decimal :precision 5}})
 
-(fact (mysql-to-clj-type [:balance "float"]) => {:balance {:type :decimal}})
-(fact (mysql-to-clj-type [:balance "decimal"]) => {:balance {:type :decimal}})
+(fact (mysql-to-clj-type [:balance "float"]) => {:balance {:type :decimal :precision 23}})
+(fact (mysql-to-clj-type [:balance "float(5,2)"]) => {:balance {:type :decimal :precision 5}})
 
-(fact (mysql-to-clj-type [:balance "float(5, 2)"]) => {:balance {:type :decimal}})
-(fact (mysql-to-clj-type [:balance "decimal(8, 3)"]) => {:balance {:type :decimal}})
+(fact (mysql-to-clj-type [:balance "decimal"]) => {:balance {:type :decimal :precision 10 :accuracy 0}})
+(fact (mysql-to-clj-type [:balance "decimal(8,3)"]) => {:balance {:type :decimal :precision 8 :accuracy 3}})
