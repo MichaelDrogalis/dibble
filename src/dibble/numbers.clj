f87d44c8ddcdefeb41552d7b23bc9be5cd94f295
Michael Drogalis <mjd3089@rit.edu>
2012-11-12 18:32:47 -0500

    Renamed some locals.

--- src/dibble/numbers.clj
+++ src/dibble/numbers.clj
@@ -2,20 +2,20 @@
   (:require [clojure.math.numeric-tower :refer :all])
   (import java.util.Random))
 
-(defn generate-value-in-range [column-description options f]
-  (let [{:keys [min max]} (merge column-description options)]
+(defn generate-value-in-range [column-map options f]
+  (let [{:keys [min max]} (merge column-map options)]
     (assert (<= min max) "Minimum bound cannot be greater than maximum bound.")
     (f min max)))
 
 (defn random-integer [min max]
   (+ (long (rand (- (inc max) min))) min))
   
-(defn randomized-integer [column-description options]
-  (generate-value-in-range column-description options random-integer))
+(defn randomized-integer [column-map options]
+  (generate-value-in-range column-map options random-integer))
 
 (defn random-double [min max]
   (+ min (* (- max min) (.nextDouble (new Random)))))
 
-(defn randomized-decimal [column-description options]
-  (generate-value-in-range column-description options random-double))
+(defn randomized-decimal [column-map options]
+  (generate-value-in-range column-map options random-double))
 
