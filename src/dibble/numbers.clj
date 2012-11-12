0d3b8c6dee891cd69804117a098858fcb3bb57d7
Michael Drogalis <mjd3089@rit.edu>
2012-11-12 18:31:53 -0500

    Refactored away duplicate code.

--- src/dibble/numbers.clj
+++ src/dibble/numbers.clj
@@ -2,19 +2,20 @@
   (:require [clojure.math.numeric-tower :refer :all])
   (import java.util.Random))
 
+(defn generate-value-in-range [column-description options f]
+  (let [{:keys [min max]} (merge column-description options)]
+    (assert (<= min max) "Minimum bound cannot be greater than maximum bound.")
+    (f min max)))
+
 (defn random-integer [min max]
   (+ (long (rand (- (inc max) min))) min))
   
 (defn randomized-integer [column-description options]
-  (let [{:keys [min max]} (merge column-description options)]
-    (assert (<= min max) "Minimum bound cannot be greater than maximum bound.")
-    (random-integer min max)))
+  (generate-value-in-range column-description options random-integer))
 
 (defn random-double [min max]
   (+ min (* (- max min) (.nextDouble (new Random)))))
 
 (defn randomized-decimal [column-description options]
-  (let [{:keys [min max]} (merge column-description options)]
-    (assert (<= min max) "Minimum bound cannot be greater than maximum bound.")
-    (random-double min max)))
+  (generate-value-in-range column-description options random-double))
 
