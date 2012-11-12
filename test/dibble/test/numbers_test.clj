baa373c2f75c11b1a28466ce28851e7775ebbc92
Michael Drogalis <mjd3089@rit.edu>
2012-11-12 17:26:01 -0500

    Cleaned up the numeric tests.

--- test/dibble/test/numbers_test.clj
+++ test/dibble/test/numbers_test.clj
@@ -2,30 +2,24 @@
   (:require [midje.sweet :refer :all]
             [dibble.numbers :refer :all]))
 
-(fact (let [n (random-integer 0 1)] (and (<= n 1) (>= n 0))) => true)
-(fact (let [n (random-integer 0 9)] (and (<= n 9) (>= n 0))) => true)
+(fact (some #{(random-integer 0 1)} (range 0 2)) =not=> nil)
+(fact (some #{(random-integer 0 9)} (range 0 10)) =not=> nil)
+(fact (some #{(random-integer -10 10)} (range -10 11)) =not=> nil)
+(fact (some #{(random-integer -10 -5)} (range -10 -4)) =not=> nil)
 
-(fact(let [n (random-integer -10 10)]
-        (and (<= n 10) (>= n -10)) => true))
-
-(fact (let [n (random-integer -10 -5)]
-        (and (<= n -5) (>= n -10)) => true))
-
-(def integer-column {:type :integer :bytes 4})
+(def integer-column {:type :integer :min -10 :max 10})
 
 (fact (>= (randomized-integer integer-column {:min 0}) 0) => true)
 (fact (>= (randomized-integer integer-column {:min 5}) 5) => true)
-
 (fact (<= (randomized-integer integer-column {:max 0}) 0) => true)
 (fact (<= (randomized-integer integer-column {:max 5}) 5) => true)
+(fact (randomized-integer integer-column {:min 5 :max 5}) => 5)
 
 (fact
  (let [n (randomized-integer integer-column {:min 5 :max 6})]
    (and (<= n 6) (>= n 5))) => true)
 
-(fact (randomized-integer integer-column {:min 5 :max 5}) => 5)
-
-(def double-column {:type :double})
+(def double-column {:type :double :min -1.0 :max 1.0})
 
 (fact (float? (randomized-decimal double-column {})) => true)
 
@@ -35,3 +29,4 @@
 (fact
  (let [n (randomized-decimal double-column {:min 1.23 :max 2.01})]
    (and (>= n 1.23) (<= n 2.01))) => true)
+
