f7f9ef5cac14ccc377b9c96f14535920be0a197a
Michael Drogalis <mjd3089@rit.edu>
2012-11-13 22:05:16 -0500

    Randomized timestamps.

--- src/dibble/time.clj
+++ src/dibble/time.clj
@@ -1,7 +1,11 @@
 (ns dibble.time
-  (:require [clj-time.coerce :refer :all])
+  (:require [dibble.numbers :refer :all]
+            [clj-time.coerce :refer :all])
   (import java.sql.Timestamp))
 
-(defn randomized-time [column-map description]
-  (Timestamp. (to-long (:max column-map))))
+(defn randomized-time [column-map options]
+  (let [{:keys [min max]} (merge column-map options)
+        min-millis (to-long min)
+        max-millis (to-long max)]
+    (Timestamp. (random-integer min-millis max-millis))))
 
