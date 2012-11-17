df8ba59914573a90138846a0dae808894dd66843
Michael Drogalis <mjd3089@rit.edu>
2012-11-17 11:35:30 -0500

    Would rather name functions 'postgres', not 'postgresql'.

--- src/dibble/postgresql.clj
+++ src/dibble/postgresql.clj
@@ -4,13 +4,10 @@
 
 (def connect-to-db (memoize #(default-connection (create-db (postgres %)))))
 
-(defn postgresql-db [args]
+(defn postgres-db [args]
   (connect-to-db (:database args))
   (let [query (exec-raw (str "select column_name, data_type from INFORMATION_SCHEMA.COLUMNS where table_name='" (name (:table args)) "'") :results)
         fields (map :column_name query)
         types (map :data_type query)]
     (partition 2 (interleave fields types))))
-
-(println (postgresql-db
- {:database {:db "postgres" :user "postgres" :password "mike" :vendor :postgres} :table :people :policy :clean-slate :n 5}))
 
