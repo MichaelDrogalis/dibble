8d30898c603f826ccb96060202d009dac4df8a6d
Michael Drogalis <mjd3089@rit.edu>
2012-11-17 11:46:34 -0500

    Postgres integral support.

--- src/dibble/postgres.clj
+++ src/dibble/postgres.clj
@@ -2,6 +2,40 @@
   (:require [korma.core :refer :all]
             [korma.db :refer :all]))
 
+(def smallint-regex #"smallint")
+(def integer-regex  #"integer")
+(def bigint-regex   #"bigint")
+
+(defn smallint-metadata [column description]
+  {(keyword column)
+   {:type :integer
+    :min -32768
+    :max 32767}})
+
+(defn integer-metadata [column description]
+  {(keyword column)
+   {:type :integer
+    :min -2147483648
+    :max 2147483647}})
+
+(defn bigint-metadata [column description]
+  {(keyword column)
+   {:type :integer
+    :min -9223372036854775808
+    :max 9223372036854775807}})
+
+(defn postgres-to-clj-type [[column data-type]]
+  (first
+   (filter
+    identity
+    (map
+     (fn [[regex metadata-fn]]
+       (if-let [description (re-matches regex data-type)]
+         (metadata-fn column description)))
+     [[smallint-regex smallint-metadata]
+      [integer-regex  integer-metadata]
+      [bigint-regex   bigint-metadata]]))))
+
 (def connect-to-db (memoize #(default-connection (create-db (postgres %)))))
 
 (defn postgres-db [args]
--- test/dibble/test/postgres_test.clj
+++ test/dibble/test/postgres_test.clj
@@ -2,3 +2,9 @@
   (:require [midje.sweet :refer :all]
             [dibble.postgres :refer :all]))
 
+(facts
+ "The Postgres smallint, integer, and bigint types are supported."
+ (fact (:type (:age (postgres-to-clj-type [:age "smallint"]))) => :integer)
+ (fact (:type (:age (postgres-to-clj-type [:age "integer"]))) => :integer)
+ (fact (:type (:age (postgres-to-clj-type [:age "bigint"]))) => :integer))
+
