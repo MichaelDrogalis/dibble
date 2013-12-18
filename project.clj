f6f90f073e3468862d45d7e4681010ae3b2c0fc2
Michael Drogalis <mjd3089@rit.edu>
2013-12-18 09:15:36 -0500

    Update deps.

--- project.clj
+++ project.clj
@@ -1,18 +1,14 @@
-(defproject dibble "0.2.0-beta1"
+(defproject dibble "0.3.0-SNAPSHOT"
   :description "A Clojure library for seeding databases"
   :url "https://github.com/MichaelDrogalis/dibble"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}  
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
-                 [midje "1.4.0"]
-                 [com.stuartsierra/lazytest "1.2.3"]
-                 [korma "0.3.0-beta11"]
+  :dependencies [[org.clojure/clojure "1.6.0-alpha3"]
+                 [korma "0.3.0-RC5"]
                  [mysql/mysql-connector-java "5.1.21"]
                  [postgresql "9.0-801.jdbc4"]
                  [org.xerial/sqlite-jdbc "3.6.16"]
-                 [cheshire "5.0.0"]
-                 [clj-time "0.4.4"]
-                 [dire "0.2.0"]]
-  :plugins [[lein-midje "2.0.4"]])
+                 [cheshire "5.3.0"]
+                 [clj-time "0.6.0"]
+                 [dire "0.5.1"]])
 
