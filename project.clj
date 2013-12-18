f4766c9bdcb0216529a8605e18a7992eaab22268
Michael Drogalis <mjd3089@rit.edu>
2013-12-18 09:48:06 -0500

    gs

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0-alpha3"]
+                 [org.clojure/data.generators "0.1.2"]
                  [korma "0.3.0-RC5"]
                  [mysql/mysql-connector-java "5.1.21"]
                  [postgresql "9.0-801.jdbc4"]
---
+++ src/dibble/data.clj
@@ -0,0 +1,48 @@
+(ns dibble.data
+  (:require [clojure.data.generators :as gen]
+            [clj-time.coerce :refer [to-long]])
+  (:import [java.util Random]
+           [java.sql Timestamp]))
+
+(defmulti random-string :subtype)
+
+(defmethod random-string :first-name [opts]
+  "Jane")
+
+(defmethod random-string :last-name [opts]
+  "Doe")
+
+(defmethod random-string :full-name [opts]
+  "Jack Doe")
+
+(defmethod random-string :default [opts]
+  (if (:length opts)
+    "Fixed len"
+    "Var len"))
+
+(defn random-integer [min max]
+  (if (and (< min 0) (< max 0))
+    (* -1 (random-integer (* -1 min) (* -1 max)))
+    (long (+ (* (rand) (inc (- (bigint max) (bigint min)))) min))))
+
+(defn random-double [min max]
+  (+ min (* (- max min) (.nextDouble (Random.)))))
+
+(defmulti generate-data (fn [constraints _] (:type constraints)))
+
+(defmethod generate-data :string [constraints args]
+  (let [opts (merge constraints args)]
+    (random-string opts)))
+
+(defmethod generate-data :integer [constraints args]
+  (let [opts (merge constraints args)]
+    (random-integer (:min opts) (:max opts))))
+
+(defmethod generate-data :decimal [constraints args]
+  (let [opts (merge constraints args)]
+    (random-double (:min opts) (:max opts))))
+
+(defmethod generate-data :inst [constraints args]
+  (let [opts (merge constraints args)]
+    (Timestamp. (random-integer (to-long (:min opts) (:max opts))))))
+
