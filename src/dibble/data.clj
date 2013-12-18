01428967cdc61ff07aa66e144b959683be68dad3
Michael Drogalis <mjd3089@rit.edu>
2013-12-18 10:00:09 -0500

    Rand str.

--- src/dibble/data.clj
+++ src/dibble/data.clj
@@ -9,6 +9,14 @@
 (def last-names
   (read-string (slurp (clojure.java.io/resource "last-names.edn"))))
 
+(defn random-integer [min max]
+  (if (and (< min 0) (< max 0))
+    (* -1 (random-integer (* -1 min) (* -1 max)))
+    (long (+ (* (rand) (inc (- (bigint max) (bigint min)))) min))))
+
+(defn random-double [min max]
+  (+ min (* (- max min) (.nextDouble (Random.)))))
+
 (defmulti random-string :subtype)
 
 (defmethod random-string :first-name [opts]
@@ -18,20 +26,17 @@
   (rand-nth last-names))
 
 (defmethod random-string :full-name [opts]
-  (str (random-string {:subtype :first-name}) " " (random-string {:subtype :last-name})))
+  (str (random-string {:subtype :first-name}) " "
+       (random-string {:subtype :last-name})))
 
 (defmethod random-string :default [opts]
-  (if (:length opts)
-    "Fixed len"
-    "Var len"))
-
-(defn random-integer [min max]
-  (if (and (< min 0) (< max 0))
-    (* -1 (random-integer (* -1 min) (* -1 max)))
-    (long (+ (* (rand) (inc (- (bigint max) (bigint min)))) min))))
-
-(defn random-double [min max]
-  (+ min (* (- max min) (.nextDouble (Random.)))))
+  (let [low-char 97
+        high-char 122
+        int-seq (repeatedly #(random-integer low-char high-char))
+        bounded-seq (if (:length opts)
+                      (take (:length opts) int-seq)
+                      (take (random-integer (:min opts) (:max opts)) int-seq))]
+    (apply str (map char bounded-seq))))
 
 (defmulti generate-data (fn [constraints _] (:type constraints)))
 
